299f0223cc2e3c72554f150d42de998f61a79d8c
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-05-31 13:29:08 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Gabriel Marquez on 2022-05-31.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
