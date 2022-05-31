57795f467c7c5f17d622e674783cef193e88c54a
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-05-31 16:32:15 -0400

    Complete day 20

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack(spacing: 0) {
+                Color.blue
+                Color.indigo
+            }
+
+            Text("Your content")
+                // .foregroundStyle
+                .foregroundColor(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+        }
+        .ignoresSafeArea()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        .preferredColorScheme(.dark)
+        .previewDevice("iPhone 11 Pro")
     }
 }
