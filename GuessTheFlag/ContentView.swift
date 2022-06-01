d1cd4370d615e2d193846dfdcfc85fd460a69ce3
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-01 16:18:59 -0400

    Day 21 revision take 3

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,11 +15,11 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        ZStack{
+        ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-            ], center: .top, startRadius: 200, endRadius: 400)
+            ], center: .top, startRadius: 200, endRadius: 650)
                 .ignoresSafeArea()
             
             VStack {
@@ -29,25 +29,15 @@ struct ContentView: View {
                     .font(.largeTitle.weight(.bold))
                     .foregroundColor(.white)
                 
-                Spacer()
-                Spacer()
-                Text("Score: ???")
-                    .foregroundColor(.white)
-                    .font(.title.bold())
-                Spacer()
-                
                 VStack (spacing: 15) {
                     VStack {
                         Text("Tap the flag of")
-                            .font(.subheadline.weight(.heavy))
                             .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
                         Text(countries[correctAnswer])
                             .font(.largeTitle.weight(.semibold))
                     }
-                    .frame(maxWidth: .infinity)
-                    .padding(.vertical, 20)
-                    .background(.regularMaterial)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
                     
                     ForEach(0..<3) { number in
                         Button {
@@ -62,15 +52,28 @@ struct ContentView: View {
                         }
                     }
                 }
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 20)
+                    .background(.regularMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    Spacer()
+                    Spacer()
+
+                    Text("Score: ???")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+
+                    Spacer()
+                }
+                .padding()
             }
-            .padding()
             .alert(scoreTitle, isPresented: $showingScore) {
                 Button("Continue", action: askQuestion)
             } message: {
                 Text("Your score is ???")
             }
         }
-    }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
@@ -91,7 +94,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .preferredColorScheme(.dark)
+            .preferredColorScheme(.light)
             .previewDevice("iPhone 11 Pro")
     }
 }
